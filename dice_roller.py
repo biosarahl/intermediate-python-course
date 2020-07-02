4b05ed4d269b4d6ddf1b2983d442ad584c98ef84
Sewon Lee <sewonl0803@gmail.com>
2020-07-02 17:42:44 +0900

    Roll twice

--- dice_roller.py
+++ dice_roller.py
@@ -1,9 +1,13 @@
 import random
+dice_rolls = 2
 
 def main():
-  roll = random.randint(1,6) 
-  print(f'You rolled a {roll}')
-  
+  dice_sum = 0
+  for i in range(0, dice_rolls):
+    roll = random.randint(1,6)
+    dice_sum += roll
+    print(f'You rolled a {roll}')
+  print(f'You rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
